feacce57fef2876a03e49ef05803b8f243b09a7f
Ushakov Ilya <Riz4rd@gmail.com>
2022-11-21 07:56:37 +0500

    Add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число = {number}");
+
+int RemoveSecondDigit(int numb)
+{
+    int firstDigit = numb / 100;
+    int thirdDigit = numb % 10;
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
+
+Console.WriteLine("Удаляем вторую цифру числа");
+Console.WriteLine($"Результат = {RemoveSecondDigit(number)}");
